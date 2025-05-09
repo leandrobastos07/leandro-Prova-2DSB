109e18631787721b2a29a62039890e612f343ab2
leandrobastos07 <titoele12@gmail.com>
2025-05-09 09:43:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function somar(){
+    let nuun1 =0;
+    let nun2 = 0;
+
+    num1 = Number(window.prompt('Digite um numero: ');
+    num2 = Number(window.prompt('Digite outro numero;'))
+       
+    }.prompt('Digite outro numero');
+
+    let soma = num1 + nun2;
+
+    alert(soma);
+
+}
